116d5e9b594b061b723d6f9080df99db22569bef
Michelle Olvera <arivas53456@ucq.edu.mx>
2026-06-07 18:47:28 -0600

    Doxygen
    Enlace Doxygen y GitHub con mi codigo de visual en C++ para comenzar el proyecto.

---
+++ html/search/all_2.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['tap1proyect_0',['TAP1Proyect',['../md__r_e_a_d_m_e.html',1,'']]],
+  ['tap1proyect_2ecpp_1',['TAP1Proyect.cpp',['../_t_a_p1_proyect_8cpp.html',1,'']]]
+];
---
+++ html/search/pages_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['tap1proyect_0',['TAP1Proyect',['../md__r_e_a_d_m_e.html',1,'']]]
+];
